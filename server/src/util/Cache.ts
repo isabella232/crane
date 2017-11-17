93126177b7e7b98a9da88194505db8be7b05bb0d
Ioan CHIRIAC <i.chiriac@yahoo.com>
2017-11-17 10:08:23 +0100

    impl the cache

--- server/src/util/Cache.ts
+++ server/src/util/Cache.ts
@@ -3,9 +3,51 @@
  * Licensed under the MIT License. See License.txt in the project root for license information.
  * "HVY", "HVY Industries" and "Hvy Industries" are trading names of JCKD (UK) Ltd
  */
+import { tmpdir } from 'os';
+import * as fs from 'fs';
+import * as path from 'path';
+
+const caches:Object = {};
 
 export default class Cache {
-    constructor(path: string) {
+    filename: string;
+    static TYPE_SHARD:string = 'shard';
+    static TYPE_INDEX:string = 'index';
 
+    constructor(filename: string) {
+      this.filename = filename;
+    }
+
+    /**
+     * Reads the cache entry
+     */
+    read(): any {
+      if (fs.existsSync(this.filename)) {
+        
+      }
+      return null;
+    }
+
+    /**
+     * Writes a cache entry
+     * @param data
+     */
+    write(data: any): void {
+
+    }
+
+    /**
+     * Retrieves the cache instance (reader & writer)
+     * @param type 
+     * @param id 
+     */
+    static instance(type: string, id: string):Cache {
+      let cacheFilename = path.join(
+        tmpdir(), 'crane', type, name
+      );
+      if (!caches.hasOwnProperty(cacheFilename)) {
+        caches[cacheFilename] = new Cache(cacheFilename);
+      }
+      return caches[cacheFilename];
     }
 }
